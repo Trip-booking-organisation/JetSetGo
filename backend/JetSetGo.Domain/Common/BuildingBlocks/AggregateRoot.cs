a646f5a67d430bf1bb5daa37a4b7b5a41e5c9e51
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2023-03-18 16:20:43 +0100

    feat: added building blocks for ddd

---
+++ backend/JetSetGo.Domain/Common/BuildingBlocks/AggregateRoot.cs
@@ -0,0 +1,13 @@
+﻿namespace HouseFinder360.Domain.Common.BuildingBlocks;
+
+public class AggregateRoot<TId> : Entity<TId> 
+    where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id)
+    {
+    }
+
+    protected AggregateRoot()
+    {
+    }
+}
---
+++ backend/JetSetGo.Domain/Common/BuildingBlocks/Entity.cs
@@ -0,0 +1,40 @@
+﻿namespace HouseFinder360.Domain.Common.BuildingBlocks;
+
+public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    where TId : notnull
+{
+    public TId Id { get; protected set; }
+
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+
+    protected Entity()
+    {
+    }
+
+    public bool Equals(Entity<TId>? other)
+    {
+        return Equals((object?) other);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Entity<TId> entity && Id.Equals(entity.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity<TId> left, Entity<TId> right)
+    {
+        return Equals(left, right);
+    }
+    public static bool operator !=(Entity<TId> left, Entity<TId> right)
+    {
+        return !Equals(left, right);
+    }
+}
