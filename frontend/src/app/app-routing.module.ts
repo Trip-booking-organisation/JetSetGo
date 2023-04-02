a1968729710e3a6a472f37eeefc03438004d0e65
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2023-04-02 23:57:05 +0200

    style

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -19,13 +19,7 @@ const routes: Routes = [
   },
   {
     path: 'flights',
-    component: FlightsComponent,
-    // canActivate: [IsAuthentificatedGuard, HasRoleGuard],
-    // data: {
-    //   role: ["Admin"
-    //     // ,"Passenger"
-    //   ]
-    //}
+    component: FlightsComponent
   },
   {
     path: 'signIn',
